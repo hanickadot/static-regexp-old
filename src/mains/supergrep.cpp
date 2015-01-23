b1adf259f2c885a9023b0a99de8b7401500e3c13
Hana Dusíková <hanicka@hanicka.net>
2015-01-23 10:32:55 +0100

    Multifile regexp.

--- src/mains/supergrep.cpp
+++ src/mains/supergrep.cpp
@@ -12,19 +12,37 @@ int main (int argc, char ** argv)
 	RegularExpression< Selection<Str<'A','B','C','D','E'>,Str<'D','E','F','G','H'>,Str<'E','F','G','H','I'>,Repeat<4,0,Chr<'A'>>>
  > regexp;
 	
-	if (argc >= 2)
+	if (argc == 2)
 	{
-	
 		std::ifstream ifs (std::string(argv[1]), std::ifstream::in);
-	
+
 		char buffer[1024];
 		//std::string line;
-	
+
 		while (ifs.getline(buffer,1024))
 		{
 			if (regexp(buffer))
 			{
 				std::cout << buffer << '\n';
+			}
+		}
+	}
+	else if (argc >= 2)
+	{
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string fname{std::string(argv[i])};
+			std::ifstream ifs (fname, std::ifstream::in);
+	
+			char buffer[1024];
+			//std::string line;
+	
+			while (ifs.getline(buffer,1024))
+			{
+				if (regexp(buffer))
+				{
+					std::cout << fname << ": " << buffer << '\n';
+				}
 			}
 		}
 	}
